347f4f1b06bf45a568975756a20a364bb8c3daee
vsmash <vsmash@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-07-24 02:02:14 +0000

    tagging 1.01
    git-svn-id: https://plugins.svn.wordpress.org/seo-alert/tags/1.01@1205389 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ readme.txt
@@ -0,0 +1,37 @@
+=== SEO ALert ===
+Contributors: vsmash
+Donate link: http://www.velvary.com.au/vanilla-beans/wordpress/seo-alert/
+Tags: SEO, bot detection, google visit, Bing visit, Yahoo Visit, sitemap scrape, search engine bot
+Requires at least: 4.0
+Tested up to: 4.23
+Stable tag: 1.01
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Monitor your website, plugins and themes.
+
+== Description ==
+This plugin send email to nominated address when the Google, Yahoo or Bing bot indexes your site.
+
+== Installation ==
+Standard Wordpress plugin installation.
+Minimum PHP version is v5.4
+Depends on wp_mail
+
+== Frequently Asked Questions ==
+
+== Screenshots ==
+
+
+== Changelog ==
+= 1.01 =
+Tested against Wordpress 4.23
+
+= 1.0 =
+Created
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+Initial Release.
---
+++ seo-alert.php
@@ -0,0 +1,77 @@
+<?php
+/*
+Plugin Name: Vanilla Bean - SEO Alert
+Plugin URI: https://wordpress.org/plugins/seo-alert/
+Description: Email report of visits from the seo bot.   --Vanilla Beans for Wordpress by Velvary Pty Ltd
+Version: 1.01
+Author: Mark Pottie <mark@pottie.com>
+Author URI: http://www.pottie.com
+License: GPLv2
+*/
+namespace VanillaBeans\SEOAlert;
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+if ( !defined( 'VBEANSEOALERT_PLUGIN_DIR' ) ) {
+	define( 'VBEANSEOALERT_PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+}
+if ( !defined( 'VBEANSEOALERT_PLUGIN_URL' ) ) {
+	define( 'VBEANSEOALERT_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
+}
+if ( !defined( 'VBEANSEOALERT_PLUGIN_FILE' ) ) {
+	define( 'VBEANSEOALERT_PLUGIN_FILE', __FILE__ );
+}
+if ( !defined( 'VBEANSEOALERT_PLUGIN_VERSION' ) ) {
+	define( 'VBEANSEOALERT_PLUGIN_VERSION', '1.01' );
+}
+
+
+$includes = array(
+	'functions.php'
+);
+
+$frontend_includes = array('request.php');
+
+
+$adminincludes= array(
+    'settings.php'
+);
+
+            // Load common includes
+            foreach ( $includes as $include ) {
+                    require_once( dirname( __FILE__ ) . '/inc/'. $include );
+            }
+            // Load admin only
+	if(is_admin()){		//load admin part
+            foreach ( $adminincludes as $admininclude ) {
+                require_once( dirname( __FILE__ ) . '/inc/admin/'. $admininclude );
+            }
+	}else{		//load front part
+            foreach ( $frontend_includes as $include ) {
+                    require_once( dirname( __FILE__ ) . '/inc/'. $include );
+            }
+	}
+
+        
+add_action('admin_menu', 'VanillaBeans\SEOAlert\vbean_seoalert_create_menu');
+
+
+if(!function_exists('vbean_seoalert_create_menu')){
+function vbean_seoalert_create_menu() {
+
+        if ( empty ( $GLOBALS['admin_page_hooks']['vanillabeans-settings'] ) ){
+                //create new top-level menu
+        	add_menu_page('Vanilla Beans', 'Vanilla Beans', 'administrator', 'vanillabeans-settings', 'VanillaBeans\LiveSettings', VBEANSEOALERT_PLUGIN_URL.'vicon.png', 4);
+            
+        }
+        add_submenu_page('vanillabeans-settings', 'SEO Alert', 'SEO Alert', 'administrator', __FILE__,'VanillaBeans\SEOAlert\SettingsPage');
+    
+
+	//call register settings function
+	add_action( 'admin_init', 'VanillaBeans\SEOAlert\RegisterSettings' );
+}
+}       
+        
